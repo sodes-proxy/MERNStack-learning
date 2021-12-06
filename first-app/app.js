996048f907c0514c85a33ac767ef318c6cbe7625
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 13:38:19 -0600

    first node app (module exercise)

---
+++ first-app/app.js
@@ -0,0 +1,3 @@
+const logger= require('./logger.js');
+//logger.log("hello world"); calling log function of logger object
+logger("hello world");
---
+++ first-app/logger.js
@@ -0,0 +1,8 @@
+var url='https://youtube.com';
+
+function log(msg){
+    console.log(msg);
+}
+module.exports=log; //exporting method
+//module.exports.log=log; exporting object with log method
+//module.exports.endPoint=url;

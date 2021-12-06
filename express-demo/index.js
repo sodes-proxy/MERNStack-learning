a63771856997e5a612dedd4a598ec9000ec873f3
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 17:25:45 -0600

    hadling post requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,11 +1,17 @@
 const express=require('express');
 const app=express();
+app.use(express.json());
 courses=[{id:1,name:'name1'},{id:2,name:'name2'},{id:3,name:'name3'}];
 app.get('/',(req,res)=>{
     res.send('hello world');
 });
 app.get('/api/courses',(req,res)=>{
     res.send(courses);
+});
+app.post('/api/courses',(req,res)=>{
+    const course={id:courses.length+1,name:req.body.name};
+    courses.push(course);
+    res.send(course);
 });
 app.get('/api/courses/:id',(req,res)=>{
     const course=courses.find(c=> c.id ===parseInt(req.params.id));

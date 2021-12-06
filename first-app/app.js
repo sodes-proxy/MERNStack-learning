b7aa5dab0c0ebf414637d188c7191737ae8c9b72
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 14:24:32 -0600

    file system module

--- first-app/app.js
+++ first-app/app.js
@@ -1,6 +1,15 @@
-const os=require('os');
-var totalMemory=os.totalmem();
-var freeMemory=os.freemem();
-console.log("total Memory: "+totalMemory);
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`free Memory: ${freeMemory}`);
+const fs=require('fs');
+console.log('syncronous');
+//synchronous
+const files=fs.readdirSync('./');
+console.log(files);
+//Async
+console.log('Asyncronous');
+fs.readdir('./',function(err,files){
+    if(err){
+        console.log('Error',err);
+    }
+    else{
+        console.log('Result',files);
+    }
+});

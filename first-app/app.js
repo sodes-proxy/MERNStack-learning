f9d1225be61b8855f5818b54a3f5533916e5142c
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 14:35:32 -0600

    event argument and arrow function

--- first-app/app.js
+++ first-app/app.js
@@ -1,8 +1,17 @@
 const EventEmiter=require('events');
 const emitter= new EventEmiter();
+/*
+* Arrow function 
+* function(arg){
+*
+* }
+(arg){
+    
+}
+*/
 //register listener
-emitter.addListener('messageLogged',function(){
-    console.log('listener called');
+emitter.addListener('messageLogged',function(arg){
+    console.log('listener called',arg);
 });
 //raise event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged',{id:1,url:'https://'});

d963ecfdc1d1605c7a7fd212cf6e2bcb3cc9d611
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 17:20:19 -0600

    handling get requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,14 +1,20 @@
 const express=require('express');
 const app=express();
+courses=[{id:1,name:'name1'},{id:2,name:'name2'},{id:3,name:'name3'}];
 app.get('/',(req,res)=>{
     res.send('hello world');
 });
 app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3]);
+    res.send(courses);
 });
 app.get('/api/courses/:id',(req,res)=>{
-    //res.send(req.params.id);
-    res.send(req.query);
+    const course=courses.find(c=> c.id ===parseInt(req.params.id));
+    if(!course){
+        res.status(404).send(`course with id ${req.params.id} was not found`);
+    }
+    else{
+        res.send(course);
+    }
 });
 const port=process.env.PORT || 3000;
 app.listen(port,()=>{

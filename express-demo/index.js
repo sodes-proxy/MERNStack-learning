3c2a0bd221c2a1d6bdd65d5f0c42b9a7344dbc52
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 17:39:01 -0600

    Input validation usin Joi

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const Joi=require('joi');
 const app=express();
 app.use(express.json());
 courses=[{id:1,name:'name1'},{id:2,name:'name2'},{id:3,name:'name3'}];
@@ -9,6 +10,12 @@ app.get('/api/courses',(req,res)=>{
     res.send(courses);
 });
 app.post('/api/courses',(req,res)=>{
+    const schema={name:Joi.string().min(3).required()};
+    const result= Joi.validate(req.body,schema);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
     const course={id:courses.length+1,name:req.body.name};
     courses.push(course);
     res.send(course);

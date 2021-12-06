a634bd7187f045a7779396adc5265638cef35f58
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 14:19:04 -0600

    OS module

--- first-app/app.js
+++ first-app/app.js
@@ -1,3 +1,6 @@
-const path= require('path');
-var pathObj =path.parse(__filename);
-console.log(pathObj);
+const os=require('os');
+var totalMemory=os.totalmem();
+var freeMemory=os.freemem();
+console.log("total Memory: "+totalMemory);
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`free Memory: ${freeMemory}`);

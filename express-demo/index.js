9b6f6b3bd4c73345440ed3cab9319dc66dc3c2c4
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 17:10:52 -0600

    path parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
 app.get('/api/courses',(req,res)=>{
     res.send([1,2,3]);
 });
+app.get('/api/courses/:id',(req,res)=>{
+    //res.send(req.params.id);
+    res.send(req.query);
+});
 const port=process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);

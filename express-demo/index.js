ed6fa007b5da9cf92957a9eaa6a9a7b749a433bf
sodes-proxy <sochoaosuna@gmail.com>
2021-12-06 18:16:18 -0600

    handling delete requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -55,6 +55,17 @@ app.get('/api/courses/:id',(req,res)=>{
         res.send(course);
     }
 });
+//deleting course
+app.delete('/api/courses/:id',(req,res)=>{
+    //validation
+    const course=courses.find(c=> c.id ===parseInt(req.params.id));
+    if(!course){
+        res.status(404).send(`course with id ${req.params.id} was not found`);
+    }
+    const index=courses.indexOf(course);
+    course.splice(index,1);
+    res.send(course);
+});
 //server listener
 const port=process.env.PORT || 3000;
 app.listen(port,()=>{

9382371def1b9463eb776a71205ca978fcfb4c4e
sodes-proxy <sochoaosuna@gmail.com>
2021-12-28 21:38:37 -0600

    component properties

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,22 +1,14 @@
 import Header from "./components/Header";
-function App() {
+const App=()=> {
   const name='sebas'
   const bool =true
   return (
     <div className="container">
-      <Header/>
+      <Header title='Hello'/>
       <h1>Hello World from {name}</h1>
       <h2>{bool?'true':'false'}</h2>
     </div>
   );
 }
-/* Example with class
-import React from 'react'
-class App extends React.Component{
-  render (){
-    return <h1>Hello from a class</h1>
-  }
-}
-*/
 
 export default App;
--- react-learning/src/components/Header.js
+++ react-learning/src/components/Header.js
@@ -1,9 +1,17 @@
-const Header = () => {
+const Header = (props) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{props.title}</h1>
         </header>
     )
 }
-
+/* destructure example
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}*/
+Header.defaultProps={title:'Task Tracker',}
 export default Header

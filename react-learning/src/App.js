7e64bc305b100e120c9a7cb00cf6521d0a740d87
sodes-proxy <sochoaosuna@gmail.com>
2021-12-29 14:12:23 -0600

    state and useState hook

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,12 +1,10 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 const App=()=> {
-  const name='sebas'
-  const bool =true
   return (
     <div className="container">
-      <Header title='Hello'/>
-      <h1>Hello World from {name}</h1>
-      <h2>{bool?'true':'false'}</h2>
+      <Header/>
+      <Tasks/>
     </div>
   );
 }
--- react-learning/src/components/Header.js
+++ react-learning/src/components/Header.js
@@ -8,7 +8,6 @@ const Header = ({title}) => {
     return (
         <header className='header'>
             <h1 style={headingStyle}>{title}</h1>
-            <Button color='green' text='hello'/>
             <Button text='add' onClick={onClick}/>
         </header>
     )
@@ -17,5 +16,5 @@ Header.defaultProps={title:'Task Tracker',}
 //defining types to catch errors easier
 Header.propTypes={title:PropTypes.string.isRequired,}
 //CSS in JS
-const headingStyle={color:'red',backgroundColor:'blue'}
+const headingStyle={backgroundColor:'lightgray'}
 export default Header

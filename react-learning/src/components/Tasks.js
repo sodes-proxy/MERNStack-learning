f527bfe93fe38bf7ad2e662aeea4434ae4777118
sodes-proxy <sochoaosuna@gmail.com>
2021-12-29 14:22:39 -0600

    Task component

--- react-learning/src/components/Tasks.js
+++ react-learning/src/components/Tasks.js
@@ -1,7 +1,8 @@
+import Task from "./Task"
 const Tasks = ({tasks}) => {
     return (
         <>
-        {tasks.map((task)=>(<h3 key={task.id}>{task.text}</h3>))}      
+        {tasks.map((task)=>(<Task key={task.id} task={task}/>))}      
         </>
     )
 }

e88c8d65c61db4d908b24d431285a67f942cbe68
sodes-proxy <sochoaosuna@gmail.com>
2021-12-28 21:32:52 -0600

    Creating and using a component

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
 function App() {
   const name='sebas'
   const bool =true
   return (
     <div className="container">
+      <Header/>
       <h1>Hello World from {name}</h1>
       <h2>{bool?'true':'false'}</h2>
     </div>
   );
 }
+/* Example with class
+import React from 'react'
+class App extends React.Component{
+  render (){
+    return <h1>Hello from a class</h1>
+  }
+}
+*/
 
 export default App;

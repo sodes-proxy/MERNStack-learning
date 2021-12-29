aad136caab9c3ae020264959bbd9e51b3ca6cec9
sodes-proxy <sochoaosuna@gmail.com>
2021-12-29 14:15:57 -0600

    global state

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import {useState} from 'react';
 const App=()=> {
+  const[tasks,setTasks]=useState([
+    {id:1,text:'first task',day:'29/12/2021',reminder:true},
+    {id:2,text:'second task',day:'30/12/2021',reminder:false}
+])
   return (
     <div className="container">
       <Header/>
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- react-learning/src/components/Tasks.js
+++ react-learning/src/components/Tasks.js
@@ -1,9 +1,4 @@
-import {useState} from 'react'
-const Tasks = () => {
-    const[tasks,setTasks]=useState([
-        {id:1,text:'first task',day:'29/12/2021',reminder:true},
-        {id:2,text:'second task',day:'30/12/2021',reminder:false}
-    ])
+const Tasks = ({tasks}) => {
     return (
         <>
         {tasks.map((task)=>(<h3 key={task.id}>{task.text}</h3>))}      
